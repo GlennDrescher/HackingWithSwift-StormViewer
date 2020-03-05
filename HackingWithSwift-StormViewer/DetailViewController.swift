cce7ad420eb2f96d7bcbf9accdab0cd980439efe
Glenn Drescher <GlennD@me.com>
2020-03-05 21:09:05 +0100

    add sharing capabilities

--- HackingWithSwift-StormViewer/DetailViewController.swift
+++ HackingWithSwift-StormViewer/DetailViewController.swift
@@ -20,10 +20,22 @@ class DetailViewController: UIViewController {
         title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found!")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
